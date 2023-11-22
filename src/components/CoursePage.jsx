234f25b867730cb82f30527c9729a787e069fdb6
Madhu Lokesh <madhumj623@gmail.com>
2023-11-22 23:59:12 +0530

    Done fetching

--- src/components/CoursePage.jsx
+++ src/components/CoursePage.jsx
@@ -1,45 +1,19 @@
-import { Button, Card, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
+import "./courseStyle.css";
 function Courses() {
-  const [courses, setCourses] = useState([]);
+  const [course, setCourse] = useState({});
   const [purchasedCourses, setPurchasedCourses] = useState([]);
   const [isPurchased, setIsPurchased] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
-  const navigate =useNavigate();
-
-  const makePayment = () => {
-    setIsLoading(true);
-    axios
-      .post(
-        `http://localhost:3000/users/courses/${id}`,
-        {},
-        {
-          headers: {
-            Authorization:
-              "Bearer " + localStorage.getItem("token"),
-          },
-        }
-      )
-      .then((res) => {
-        toast.success(res.data.message);
-        setPurchasedCourses([
-          ...purchasedCourses,
-          res.data.purchasedCourse,
-        ]);
-        setIsPurchased(true);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  }
-  
+  console.log("coursepage", id)
+  console.log("coursebuy", course)
   useEffect(() =>{
+    setIsLoading(true);
+
       axios.get(
         `http://localhost:3000/users/courses/${id}`,
           {
@@ -49,9 +23,11 @@ function Courses() {
           }
         )
          .then((res) => {
-          setCourses(res.data.courses);
+          setCourse(res.data.course);
+          console.log("data.course", res.data.course)
          })
-         .catch((err) => console.log(err))
+         .catch((err) => console.log(err));
+
 
          axios.get("http://localhost:3000/users/purchasedCourses", {
               headers: {
@@ -67,6 +43,7 @@ function Courses() {
             }, [id]);
 
 useEffect(() =>{
+   // Check if the current course is purchased
   const ans = purchasedCourses.some((item) => item._id === id);
   setIsPurchased(ans)
 }, [id, purchasedCourses]);
@@ -89,21 +66,21 @@ if (isLoading) {
   )
 }
 return(
-    <div className="course">
+    <div className="single-course">
       <div className="text-container">
         <div>
-          <img src={courses.imageLink}
-          alt={courses.imageLink}
+          <img src={course?.imageLink}
+                alt={course?.imageLink}
           width={"200px"}
           style={{ borderRadius : "20px"}} 
           />
         </div>
         <div>
-          <h1 className="course-title">{courses.title}</h1>
+          <h1 className="course-title">{course?.title}</h1>
         </div>
         <br></br>
         <div>
-          <h3 className="description">{courses.description}</h3>
+          <h3 className="description">{course?.description}</h3>
         </div>
 
         <div>
@@ -116,9 +93,35 @@ return(
               fontWeight: "500",
               fontSize: "50px"
             }}
-            onClick={makePayment}
+            onClick={() => {
+              setIsLoading(true);
+              axios
+                .post(
+                  `http://localhost:3000/users/courses/${id}`,
+                  {},
+                  {
+                    headers: {
+                      Authorization:
+                        "Bearer " + localStorage.getItem("token"),
+                    },
+                  }
+                )
+                .then((res) => {
+                  toast.success(res.data.message);
+                  setPurchasedCourses([
+                    ...purchasedCourses,
+                    res.data.purchasedCourse,
+                  ]);
+                  setIsPurchased(true);
+                  setIsLoading(false);
+                })
+                .catch((err) => {
+                  console.log(err);
+                  setIsLoading(false);
+                });
+            }}
             >
-              BUY NOW @${courses.price}
+              BUY NOW @${course?.price}
             </Button>
           ) : (
             <div><Button
--- src/components/Purchased.jsx
+++ src/components/Purchased.jsx
@@ -1,57 +1,65 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Typography } from "@mui/material";
+import { CircularProgress, Skeleton, Typography } from "@mui/material";
 import "./courseStyle.css";
-import { CourseCard } from "./CourseCard"; // Update the path as needed
 
-function PurchasedCourses(){
- const [purCourses, setPurchasedCourses] = useState([]);
- const [isLoading, setIsLoading] = useState(true);
+function PurchasedCourses() {
+  const [purCourses, setPurchasedCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
- useEffect( () =>{
-  axios.get("http://localhost:3000/users/purchasedCourses", {
-    headers:{
-      Authorization: "Bearer " + localStorage.getItem("token")
-    },
-  })
-  .then((res) =>{
-    setPurchasedCourses(res.data.purchasedCourses);
-    setIsLoading(false);
-  })
-  .catch((err) =>{
-    console.log(err)
-    setIsLoading(false);
-  });
- }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get("http://localhost:3000/users/purchasedCourses", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        setPurchasedCourses(res.data.purchasedCourses);
+        console.log("data.purchasedCourses", res.data.purchasedCourses);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
 
- return (
-  <div>
-  <Typography
-    variant="h4"
-    component="div"
-    style={{
-      flexGrow: 1,
-      padding: "10px",
-      borderRadius: "4px",
-      fontWeight: "bold",
-      color: "#101460",
-      textAlign: "center",
-      marginTop: "70px",
-    }}
-  >
-    Purchased Courses
-  </Typography>
-  <div className="all-courses">
-    {purCourses.length > 0
-      ? purCourses.map((course) => (
-        <CourseCard key={course._id} course={course} />
-      ))
-      : isLoading
-        ? <CircularProgress size="sm" color="neutral" />
-        : "No course has yet been purchased"
-    }
-  </div>
-  </div> 
+  return (
+    <div>
+      <Typography
+        variant="h4"
+        component="div"
+        style={{
+          flexGrow: 1,
+          padding: "10px",
+          borderRadius: "4px",
+          fontWeight: "bold",
+          color: "#101460",
+          textAlign: "center",
+          marginTop: "70px",
+        }}
+      >
+        Purchased Courses
+      </Typography>
+      <div className="all-courses">
+        {isLoading ? (
+          <div style={{ display: "flex", gap: "20px" }}>
+            <Skeleton variant="rectangular" width={345} height={400} />
+            <Skeleton variant="rectangular" width={345} height={400} />
+            <Skeleton variant="rectangular" width={345} height={400} />
+          </div>
+        ) : (
+          <>
+            {purCourses.length > 0
+              ? purCourses.map((course) => (
+                  <CourseCard key={course._id} course={course} />
+                ))
+              : "No course has yet been bought!"}
+          </>
+        )}
+      </div>
+    </div>
   );
 }
 
